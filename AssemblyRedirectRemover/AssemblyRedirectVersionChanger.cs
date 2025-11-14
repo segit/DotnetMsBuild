d7d4be799e61fede5de61d4030c667f56606b3b5
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2025-11-14 10:16:25 -0800

    version changer

---
+++ AssemblyRedirectRemover/AssemblyRedirectVersionChanger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyRedirectRemover
+{
+    internal static class AssemblyRedirectVersionChanger
+    {
+        // TODO:
+        /// <summary>
+        /// Modifies the assembly binding redirect in the specified configuration file.
+        /// Example:
+        ///     Original Redirect:
+        ///           <dependentAssembly>
+        ///             <assemblyIdentity name = "System.ValueTuple" publicKeyToken="cc7b13ffcd2ddd51" culture="neutral" />
+        ///             <bindingRedirect oldVersion = "0.0.0.0-4.0.3.0" newVersion="4.0.3.0" />
+        ///           </dependentAssembly>
+        ///     Resulting Redirect
+        ///           <dependentAssembly>
+        ///             <assemblyIdentity name = "System.ValueTuple" publicKeyToken="cc7b13ffcd2ddd51" culture="neutral" />
+        ///             <bindingRedirect oldVersion = "0.0.0.0-4.0.5.0" newVersion="4.0.0.0" />
+        ///           </dependentAssembly>
+        ///     Parameters:
+        ///         assemblyShortName = "System.ValueTuple"
+        ///         range = "0.0.0.0-4.0.5.0"
+        ///         newVersion = "4.0.0.0"
+        /// </summary>
+        /// <remarks>This method updates the assembly binding redirect in the configuration file located
+        /// at the specified path. Ensure that the file is accessible and writable before calling this method.</remarks>
+        /// <param name="path">The file path to the configuration file where the assembly redirect will be changed.</param>
+        /// <param name="assemblyShortName">The short name of the assembly for which the redirect is being modified.</param>
+        /// <param name="range">The version range that the redirect applies to.</param>
+        /// <param name="redirect">The new version to which the assembly should be redirected.</param>
+        internal static void ChangeAssemblyRedirect(this string path, string assemblyShortName, string range, string redirect)
+        {
+        }
+    }
+}
---
+++ AssemblyRedirectRemover/ConfigFilesTraverser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyRedirectRemover
+{
+    internal static class ConfigFilesTraverser
+    {
+        /// <summary>
+        /// GetConfigFiles loop through directory
+        /// and return all "*app.config*" and "*web.config*" files 
+        /// excluding the ones in bin or obj directories.
+        /// .git .vs .vscode directories should also be skipped
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        internal static Task<IEnumerable<string>> GetConfigFiles(this string path)
+        {
+            var excludedDirectories = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                    "bin", "obj", ".git", ".vs", ".vscode"
+            };
+
+            var result = GetConfigFilesWithSearchOption(path, excludedDirectories);
+            return Task.FromResult(result);
+        }
+
+        private static IEnumerable<string> GetConfigFilesWithSearchOption(string directoryPath, HashSet<string> excludedDirectories)
+        {
+            var configFiles = new List<string>();
+
+            try
+            {
+                // Check if directory exists
+                if (!Directory.Exists(directoryPath))
+                    return configFiles;
+
+                // Get all config files using SearchOption.AllDirectories
+                var allConfigFiles = Directory.GetFiles(directoryPath, "*config*", SearchOption.AllDirectories)
+                .Where(file =>
+                {
+                    var fileName = Path.GetFileName(file).ToLowerInvariant();
+                    return fileName.Contains("app.config", StringComparison.OrdinalIgnoreCase)
+                    || fileName.Contains("web.config", StringComparison.OrdinalIgnoreCase);
+                })
+                  .Where(file =>
+                  {
+                      // Filter out files in excluded directories
+                      var pathParts = Path.GetDirectoryName(file)?.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar) ?? Array.Empty<string>();
+                      return !pathParts.Any(part => excludedDirectories.Contains(part));
+                  });
+
+                configFiles.AddRange(allConfigFiles);
+            }
+            catch
+            {
+                throw;
+            }
+
+            return configFiles;
+        }
+    }
+}
