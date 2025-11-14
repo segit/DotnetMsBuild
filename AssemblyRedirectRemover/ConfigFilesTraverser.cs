20fe1e00de7507078180eca1e16116093bcd99d4
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2025-11-14 11:33:14 -0800

    version changer test

--- AssemblyRedirectRemover/ConfigFilesTraverser.cs
+++ AssemblyRedirectRemover/ConfigFilesTraverser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AssemblyRedirectRemover
+﻿namespace AssemblyRedirectRemover
 {
     internal static class ConfigFilesTraverser
     {
@@ -13,8 +7,7 @@ namespace AssemblyRedirectRemover
         /// and return all "*app.config*" and "*web.config*" files 
         /// excluding the ones in bin or obj directories.
         /// .git .vs .vscode directories should also be skipped
-        /// TODO:
-        /// add two specific files to the list 
+        /// Also includes two specific files:
         /// Fusion.One\Shell\Telexy.Fusion.Shell\Config\Telexy.Fusion.App.Deployer.exe.config
         /// Fusion.One\Shell\Telexy.Fusion.Shell\WebConfig.Master.template
         /// </summary>
@@ -24,7 +17,7 @@ namespace AssemblyRedirectRemover
         {
             var excludedDirectories = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
             {
-                    "bin", "obj", ".git", ".vs", ".vscode"
+                "bin", "obj", ".git", ".vs", ".vscode"
             };
 
             var result = GetConfigFilesWithSearchOption(path, excludedDirectories);
@@ -43,20 +36,35 @@ namespace AssemblyRedirectRemover
 
                 // Get all config files using SearchOption.AllDirectories
                 var allConfigFiles = Directory.GetFiles(directoryPath, "*config*", SearchOption.AllDirectories)
-                .Where(file =>
-                {
-                    var fileName = Path.GetFileName(file).ToLowerInvariant();
-                    return fileName.Contains("app.config", StringComparison.OrdinalIgnoreCase)
-                    || fileName.Contains("web.config", StringComparison.OrdinalIgnoreCase);
-                })
-                  .Where(file =>
+                 .Where(file =>
                   {
-                      // Filter out files in excluded directories
-                      var pathParts = Path.GetDirectoryName(file)?.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar) ?? Array.Empty<string>();
-                      return !pathParts.Any(part => excludedDirectories.Contains(part));
-                  });
+                      var fileName = Path.GetFileName(file).ToLowerInvariant();
+                      return fileName.Contains("app.config", StringComparison.OrdinalIgnoreCase)
+                       || fileName.Contains("web.config", StringComparison.OrdinalIgnoreCase);
+                  })
+                    .Where(file =>
+               {
+                   // Filter out files in excluded directories
+                   var pathParts = Path.GetDirectoryName(file)?.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar) ?? Array.Empty<string>();
+                   return !pathParts.Any(part => excludedDirectories.Contains(part));
+               });
 
                 configFiles.AddRange(allConfigFiles);
+
+                // Add specific files mentioned in the requirements
+                var specificFiles = new[]
+                {
+                    Path.Combine(directoryPath, "Fusion.One", "Shell", "Telexy.Fusion.Shell", "Config", "Telexy.Fusion.App.Deployer.exe.config"),
+                    Path.Combine(directoryPath, "Fusion.One", "Shell", "Telexy.Fusion.Shell", "WebConfig.Master.template")
+                };
+
+                foreach (var specificFile in specificFiles)
+                {
+                    if (File.Exists(specificFile) && !configFiles.Contains(specificFile))
+                    {
+                        configFiles.Add(specificFile);
+                    }
+                }
             }
             catch
             {
