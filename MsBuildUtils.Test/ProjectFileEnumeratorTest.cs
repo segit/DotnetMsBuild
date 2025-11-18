d2fb98833729145bef888a662fa588db4b84e9b0
Sergey Ermolenko <sergey.ermolenkoo@gmail.com>
2025-11-17 19:25:51 -0800

    enumerating projects as file info

--- MsBuildUtils.Test/ProjectFileEnumeratorTest.cs
+++ MsBuildUtils.Test/ProjectFileEnumeratorTest.cs
@@ -11,4 +11,15 @@ public class ProjectFileEnumeratorTest
         Assert.Contains("Ase.Abstractions/Ase.Abstractions.csproj", list);
         Assert.Contains("ConsoleApp/ConsoleApp.csproj", list); 
     }
+
+    [Fact]
+    public void CanEnumerateSlnxAsFi()
+    {
+        var list = ProjectFilesEnumerator.EnumerateAsFi("SampleData/Ase.WebApi.slnx");
+        var proj1 = list.Where(fi => fi.Name == "Ase.Cmd.csproj").Single();
+        Assert.True(proj1.Exists);
+
+        var proj2 = list.Where(fi => fi.Name == "Ase.Ef.Core.csproj.csproj").Single();
+        Assert.False(proj1.Exists);
+    }
 }
--- MsBuildUtils/ProjectFilesEnumerator.cs
+++ MsBuildUtils/ProjectFilesEnumerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml.Linq;
+using System.IO;
 
 namespace MsBuildUtils;
 
@@ -7,7 +8,7 @@ public static class ProjectFilesEnumerator
     /// <summary>
     /// Enumerates the project files within the specified solution.
     /// </summary>
-    /// <param name="solution">The path to the solution file in slnx format. Must be a valid file path.</param>
+    /// <param name="solution">The path to the solution file in the slnx format. Must be a valid file path.</param>
     /// <returns>An enumerable collection of project file paths contained in the solution.</returns>
     public static IEnumerable<string> Enumerate(string solution)
     {
@@ -16,5 +17,20 @@ public static class ProjectFilesEnumerator
         return doc.Descendants("Project")
                   .Select(e => e.Attribute("Path")?.Value)
                   .Where(path => !string.IsNullOrEmpty(path))!;
+    }
+
+    /// <summary>
+    /// Enumerates the project files within the specified solution.
+    /// </summary>
+    /// <param name="solution">The path to the solution file in the slnx format. Must be a valid file path.</param>
+    /// <returns>An enumerable collection of FileInfo objects representing project file paths contained in the solution.</returns>
+    public static IEnumerable<FileInfo> EnumerateAsFi(string solution)
+    {
+        var doc = XDocument.Load(solution);
+        var solutionDir = Path.GetDirectoryName(solution)!;
+        return doc.Descendants("Project")
+                  .Select(e => e.Attribute("Path")?.Value)
+                  .Where(path => !string.IsNullOrEmpty(path))
+                  .Select(path => new FileInfo(Path.Combine(solutionDir, path!)));
     }
 }
