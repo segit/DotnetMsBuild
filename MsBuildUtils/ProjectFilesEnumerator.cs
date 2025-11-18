8fea18e5fc87f09b1f6713f746715a8a1b31a78d
Sergey Ermolenko <sergey.ermolenkoo@gmail.com>
2025-11-17 19:10:53 -0800

    project files enumerator

--- MsBuildUtils/ProjectFilesEnumerator.cs
+++ MsBuildUtils/ProjectFilesEnumerator.cs
@@ -1,16 +1,20 @@
-﻿namespace MsBuildUtils;
+﻿using System.Xml.Linq;
+
+namespace MsBuildUtils;
 
 public static class ProjectFilesEnumerator
 {
-    //TODO
     /// <summary>
     /// Enumerates the project files within the specified solution.
-    /// e.g. 
     /// </summary>
     /// <param name="solution">The path to the solution file in slnx format. Must be a valid file path.</param>
     /// <returns>An enumerable collection of project file paths contained in the solution.</returns>
     public static IEnumerable<string> Enumerate(string solution)
     {
-        throw new NotImplementedException();
+        var doc = XDocument.Load(solution);
+        // Find all <Project Path="..." /> elements anywhere in the document
+        return doc.Descendants("Project")
+                  .Select(e => e.Attribute("Path")?.Value)
+                  .Where(path => !string.IsNullOrEmpty(path))!;
     }
 }
