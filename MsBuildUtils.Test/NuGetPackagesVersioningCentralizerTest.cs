b2e5d8c8e0be73e08362ade00ffb98604a542bdf
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2025-11-28 11:31:11 -0800

    assingment in NugetPmCentralyzer.cs

---
+++ MsBuildUtils.Test/NuGetPackagesVersioningCentralizerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MsBuildUtils.Test
+{
+    public class NuGetPackagesVersioningCentralizerTest
+    {
+        //TODO: 
+        //  test the following method:
+        //  NugetPmCentralyzer.MoveNugetPackageVersionsToDirectoryPackagetPropsProps
+        // Test data:
+        //  Project file sample:
+        //      Test\SampleData\Ase.Cmd\Ase.Cmd.csproj
+        //  Directory.Packages.props file sample:
+        //      SampleData\Directory.Packages.props
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void CanCentralizePackagesVersioning()
+        {
+        }
+    }
+}
--- MsBuildUtils/NugetPmCentralyzer.cs
+++ MsBuildUtils/NugetPmCentralyzer.cs
@@ -6,16 +6,32 @@ namespace MsBuildUtils
 {
     public class NugetPmCentralyzer
     {
+        // TODO:
+        // Implement the method as described.
+        // Then implement the test case as described in TODO section 
+        // in the 
+        // MsBuildUtils.Test\NuGetPackagesVersioningCentralizerTest.cs
+
+
         /// <summary>
-        /// Given the name of the csproj project file
-        /// Find Project File sample
-        /// MsBuildUtils.Test\SampleData\Ase.Cmd\Ase.Cmd.csproj
+        /// Prepares project to switch to Centralized Package Management (CPM)
+        /// https://learn.microsoft.com/en-us/nuget/consume-packages/central-package-management
+        /// 
+        /// Given the path to the csProj project file and 
+        /// Directory.Packages.props file
+        /// it loops through all PackageReference elements present in project file.
+        /// Fore each PackageReference it adds PackageVersion element
+        /// to the Directory.Packages.props file if the one does not exists yet.
+        /// Examples of elements
+        ///     PackageReference element:
+        ///         <PackageReference Include="SomeRandomPackage" Version="X.X.XX" />
+        ///     PackageVersion elemement:
+        ///         <PackageVersion Include="SomeRandomPackage" Version="X.X.XX" />
         /// </summary>
-        /// <param name="proj"></param>
-        /// <param name="packagesProps"></param>
+        /// <param name="csProj">Path to MsBuild SDK style .csproj file</param>
+        /// <param name="packagesProps">Path MsBuild Directory.Packages.props</param>
         /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public static bool MoveNugetPackageVersionsToDirectoryPackagetPropsProps(string proj, string packagesProps)
+        public static bool MoveNugetPackageVersionsToDirectoryPackagetPropsProps(string csProj, string packagesProps)
         {
             throw new NotImplementedException();
         }
