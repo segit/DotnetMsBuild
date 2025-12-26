ac7381b68f00b0db1bf638f82659ccf5b6fbdd3d
Sergey Ermolenkom <sergey.ermolenkoo@gmail.com>
2025-12-26 13:09:14 -0800

    NugetPmSlnRefCentralizer bug fix: project references versions not removed when already in Directory.Packages.Props file

--- AssemblyRedirectRemover/Program.cs
+++ AssemblyRedirectRemover/Program.cs
@@ -14,6 +14,9 @@ var lf = host.Services.GetRequiredService<IHostApplicationLifetime>();
 string? asep = Environment.GetEnvironmentVariable("asep");
 ArgumentException.ThrowIfNullOrWhiteSpace(asep);
 string sln = Path.GetFullPath($"{asep}/src/Ase.WebApi.slnx");
-DotnetVersionUpdaterM
-    .UpdateAllDotnetVersionsTo(sln, new string[] { "8.0", "9.0" }, "net10.0");
+//DotnetVersionUpdaterM
+//    .UpdateAllDotnetVersionsTo(sln, new string[] { "8.0", "9.0" }, "net10.0");
+new NugetPmSlnRefCentralizer()
+    .MovePackageVersionsToCentralStore(sln);
+
 Console.WriteLine("Done!");
