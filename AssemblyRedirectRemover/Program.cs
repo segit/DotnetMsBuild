aa08b72b2aaa96e5e0ab671e73daba291c6f31d9
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2026-01-06 16:01:47 -0800

    fix program

--- AssemblyRedirectRemover/Program.cs
+++ AssemblyRedirectRemover/Program.cs
@@ -16,11 +16,14 @@ var lf = host.Services.GetRequiredService<IHostApplicationLifetime>();
 //string sln = Path.GetFullPath($"{asep}/src/Ase.WebApi.slnx");
 //DotnetVersionUpdaterM
 //    .UpdateAllDotnetVersionsTo(sln, new string[] { "8.0", "9.0" }, "net10.0");
-/ new NugetPmSlnRefCentralizer()
+
+// new NugetPmSlnRefCentralizer()
 //    .MovePackageVersionsToCentralStore(sln);
-string? wsp = Environment.GetEnvironmentVariable("wsp8");
-ArgumentException.ThrowIfNullOrWhiteSpace(wsp);
-string path = Path.Combine(wsp, "Fusion.One");
-DirectoryInfo dir = new DirectoryInfo(path);
-AssemblyRedirectsRemover.RemoveAssemblyRedirectsInDirectory(path, "Microsoft.Extensions.AI");
+
+//string? wsp = Environment.GetEnvironmentVariable("wsp8");
+//ArgumentException.ThrowIfNullOrWhiteSpace(wsp);
+//string path = Path.Combine(wsp, "Fusion.One");
+//DirectoryInfo dir = new DirectoryInfo(path);
+//AssemblyRedirectsRemover.RemoveAssemblyRedirectsInDirectory(path, "Microsoft.Extensions.AI");
+
 Console.WriteLine("Done!");
